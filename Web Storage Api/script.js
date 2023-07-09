2178176f5fdfa0d5e17aeac36eec967fa9f149ec
Moneer Kamal <moneerkamalprog@gmail.com>
2023-07-09 15:45:05 +0300

    web storage api

---
+++ Web Storage Api/script.js
@@ -0,0 +1,11 @@
+// Web Storage API
+
+// Not Part of the DOM, refers to the window API
+// available to js via the gloable variable window
+
+const myarray = ["item1", "item2", "item3"];
+
+const myobject = {
+  name: "moneer",
+  arr: ["item1", "item2", "item3"],
+};
